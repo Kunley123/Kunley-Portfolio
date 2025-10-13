d53f8085df8f56900c6a45dbe9f413271ea1e9a9
Adebanjo Adekunle <Adebanjokunle56@gmail.com>
2025-10-13 21:50:28 +0100

    Changed public key

--- src/components/section/Contact.jsx
+++ src/components/section/Contact.jsx
@@ -11,8 +11,9 @@ export const Contact = () => {
     });
 
 
-    const hanldeSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
+        console.log("submitted")
 
         emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_PUBLIC_KEY).then((result) => {
             alert("Messsage Sent!")
@@ -24,7 +25,7 @@ export const Contact = () => {
         <RevealOnScroll>
             <div className="max-w-sm md:max-w-lg flex flex-col px-4 w-150">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-600 to-cyan-300 bg-clip-text text-transparent text-center">Get In Touch</h2>
-                <form className="space-y-6" onSubmit={hanldeSubmit}>
+                <form className="space-y-6" onSubmit={handleSubmit}>
                     <div className="relative">
                         <input type="text" id="name" name="name" required value={formData.name} className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition  focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" placeholder="Name..." onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
                     </div>
@@ -37,7 +38,7 @@ export const Contact = () => {
                         <textarea id="message" name="message" required rows={5} value={formData.message} className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" placeholder="Your Message..." onChange={(e) => setFormData({ ...formData, message: e.target.value })} />
                     </div>
 
-                    <button type="submit" className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overfloe-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4">Send Message</button>
+                    <button type="submit" className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4">Send Message</button>
 
                 </form>
             </div>
--- src/components/section/Project.jsx
+++ src/components/section/Project.jsx
@@ -7,7 +7,7 @@ export const Project = () => {
         <RevealOnScroll>
             <div className="max-w-5xl mx-auto px-4">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-600 to-cyan-300 bg-clip-text text-transparent text-center">Featured Projects</h2>
-                <div className="grid grid-cold-1 md:grid-cols-2 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 2246,0.2)]">
                         <h3 className="text-xl font-bold mb-2">E-commerce Platform</h3>
                         <p className="text-gray mb-4">Commercial platform for selling of products</p>
